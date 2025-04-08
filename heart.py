d63623c10878e992c0a9ccb60e44017b8ec803f4
Waaaasd <serga00766@gmail.com>
2025-04-09 00:07:17 +0300

    Update heart.py

--- heart.py
+++ heart.py
@@ -19,7 +19,6 @@ try:
     t.pendown()
     t.color("white")
     t.write("°.• For:  •                                            • From: •.°", align="center", font=("Arial", 16, "normal"))
-
     
     # Рисуем фигуру
     for i in range(2550):
